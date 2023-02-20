bfd84d66dd0ecb48980307167cf3007718e07e47
thisara234 <68735430+thisara234@users.noreply.github.com>
2023-02-20 21:08:28 +0530

    howdy and google locators added
    need to add more to howdy

---
+++ locators/locators.py
@@ -0,0 +1,24 @@
+#google
+SearchBarLocator = "//div[@class='YacQv']"
+FirstSearchResultLocator = "//div[@class='yuRUbf']"
+
+#howdy
+trojanVpnHeaderLocator = "//div[@id='navbarCollapse2'] //li[@class='nav-item'] //a[text()='Trojan VPN']"
+SecondpageLocator = "//a[@class='page-link']  [text()='2']"
+ThirdpageLocator = "//a[@class='page-link']  [text()='3']"
+FourthpageLocator = "//a[@class='page-link']  [text()='4']"
+FifthpageLocator = "//a[@class='page-link']  [text()='5']"
+SGHEMUXTrojanVPNButtonLocator = "//a[@title='SG-HE MUX Trojan VPN'] "
+SGHEMUXTrojanVPNTxtLocator = "//h3[text()='SG-HE MUX Trojan VPN']"
+SGHE3TrojanVPNButtonLocator = "//a[@title='SG-HE 3 Trojan VPN']"
+SGHE3TrojanVPNTxtLocator = "//h3[text()='SG-HE 3 Trojan VPN']"
+SGHE2TrojanVPNButtonLocator = "//a[@title='SG-HE 2 Trojan VPN']"
+SGHE2TrojanVPNTxtLocator = "//h3[text()='SG-HE 2 Trojan VPN']"
+SGTrojanOVHButtonLocator = "//a[@title='SG Trojan OVH']"
+SGTrojanOVHTxtLocator = "//h3[text()='SG Trojan OVH']"
+
+
+
+
+
+
